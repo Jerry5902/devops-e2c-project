2d310b79c86cdd07862cae5dd98d2880ad269bbb
Ubuntu <ubuntu@ip-172-31-36-128.eu-north-1.compute.internal>
2026-02-02 22:12:21 +0000

    Fix app and docker-compose after deployment issues

--- app/app.js
+++ app/app.js
@@ -1,1 +1,12 @@
-Hello from CI/CD ðŸš€
+const http = require("http");
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello from CI/CD íº€");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
